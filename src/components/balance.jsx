30f97f2ba0f05672593756a3783a603924e03c8d
rtapia1 <rtapia@ucsd.edu>
2021-09-04 01:36:11 -0700

    intial ( set up of default app appearence )

---
+++ src/components/balance.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function balance() {
+    return (
+        <div>
+            <h4> Balance </h4>
+            <h1 id="balance"> $0.00 </h1>
+        </div>
+    )
+}
